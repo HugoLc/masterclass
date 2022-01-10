c1c2c155dd410d2fa84097f59232ab6e208bc71b
Hugo LC <hugolindoso.c@gmail.com>
2022-01-10 16:55:39 -0300

    utilizando css no next.js usando mais de uma classe sendo uma das classes no arquivo global

--- fundamentos/Notes.md
+++ fundamentos/Notes.md
@@ -2,3 +2,4 @@
 
 - so de criar um arquivo dentro da página pages automaticamente é criada uma rota para essa url.
 
+- os css globais são importados dentro do arquivo _app.jsx
---
+++ fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,8 @@
+import styles from "../styles/Estiloso.module.css";
+export default function Estiloso(){
+    return (
+        <>
+            <h1 className={`${styles.roxo} center`}>Estilo usando css modularizado</h1>
+        </>
+    )
+} 
