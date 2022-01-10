1d188fb792165c07151daee9558ac3000ca9f1f9
Hugo LC <hugolindoso.c@gmail.com>
2022-01-10 15:47:39 -0300

    sobre páginas e rotas

---
+++ fundamentos/Notes.md
@@ -0,0 +1,4 @@
+# ANOTAÇÕES #
+
+- so de criar um arquivo dentro da página pages automaticamente é criada uma rota para essa url.
+
---
+++ fundamentos/src/pages/teste.jsx
@@ -0,0 +1,6 @@
+export default function Teste(){
+    return "teste"
+}
+
+// so de criar um arquivo dentro da página pages automaticamente é criada uma rota
+// para essa url http://localhost:3000/teste
