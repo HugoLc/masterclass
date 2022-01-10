f431e47ccd22a7149e74d7686dacab07cc8387fb
Hugo LC <hugolindoso.c@gmail.com>
2022-01-10 17:36:54 -0300

    criando e chamado um component

---
+++ fundamentos/src/components/Cabecalho.jsx
@@ -0,0 +1,7 @@
+export default function Cabecalho(){
+    return (
+        <>
+            <h1>Este cabecalho é um componente</h1>
+        </>
+    )
+}
--- fundamentos/src/pages/index.jsx
+++ fundamentos/src/pages/index.jsx
@@ -1,7 +1,10 @@
+import Cabecalho from "../components/Cabecalho";
+
 export default function Inicio(){
     return (
         <>
             <h1>Título</h1>
+            <Cabecalho />
             <p>Esse é meu index em <strong>Next.js</strong></p>
         </>
         
