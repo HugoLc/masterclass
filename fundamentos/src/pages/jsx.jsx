f563713bb7bd816b5dfd3a6d4b76e6958a39db94
Hugo LC <hugolindoso.c@gmail.com>
2022-01-10 16:22:55 -0300

    chamando função dentro do jsx

--- fundamentos/src/pages/jsx.jsx
+++ fundamentos/src/pages/jsx.jsx
@@ -1,9 +1,13 @@
 export default function Jsx(){
     const titulo = <h1><strong>Jsx</strong> é um conceito central</h1>
+    const subTitulo = () =>{
+        return <h2> E é muito legal</h2>
+    }
 
     return(
         <>
             {titulo}
+            {subTitulo()}
         </>
     )
 }
