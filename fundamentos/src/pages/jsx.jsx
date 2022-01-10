4764fd0d8115cb437640e898588bccda92e609bf
Hugo LC <hugolindoso.c@gmail.com>
2022-01-10 16:14:54 -0300

    sobre armazenar jsx em uma variavel js

--- fundamentos/src/pages/jsx.jsx
+++ fundamentos/src/pages/jsx.jsx
@@ -1,11 +1,9 @@
 export default function Jsx(){
-    const a = 11;
-    const b = 5;
+    const titulo = <h1><strong>Jsx</strong> é um conceito central</h1>
 
     return(
         <>
-            <h1><strong>Jsx</strong> é um conceito central</h1>
-            {(a * b)/ Math.random()}
+            {titulo}
         </>
     )
 }
