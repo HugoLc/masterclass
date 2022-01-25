149d23d9590e0eca7b9389b850f2e12127eadf38
Hugo LC <hugolindoso.c@gmail.com>
2022-01-25 20:04:02 -0300

    tratando requisições pela url

--- fundamentos/src/pages/api/hello.js
+++ fundamentos/src/pages/api/hello.js
@@ -4,6 +4,10 @@ export default function handler(req, res) {
   res.status(200).json(
     { 
       name: 'John Doe',
-      method: req.method
+      method: req.method,
+      // http://localhost:3000/api/hello?nome=tobias&idade=20
+      //params: JSON.stringify(req.query)
+      nome: req.query.nome,
+      idade: req.query.idade
     })
 }
